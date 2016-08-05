e41038e9e5e328348c17dd157b5e1a908ca6616a
Deamond12333 <andrei-587@mail.ru>
2016-08-05 13:52:02 +0600

    v 0.1. Search ports in a sepatare thread.

--- MainWindow.cs
+++ MainWindow.cs
@@ -14,18 +14,21 @@ namespace PortScanner
     public partial class MainWindow : Form
     {
         public static ManualResetEvent connectDone = new ManualResetEvent(false);
+        public bool isStart = false;
+        public Thread scanThread;
         public MainWindow()
         {
-
             InitializeComponent();
             hostBox.Text = "127.0.0.1";
+            scanThread = new Thread(Scan);
+            scan_button.Text = "Сканировать";
         }
 
         public void Scan()
         {
             int StartPort = Convert.ToInt32(portsTo.Value);
             int EndPort = Convert.ToInt32(portsFrom.Value);
-            int i;
+            int i, j = 0;
 
             if (isScanAllPorts.Checked)
             {
@@ -40,42 +43,55 @@ namespace PortScanner
                 EndPort = buffer;
             }
 
-            progress_bar.Maximum = EndPort - StartPort + 1;
-            progress_bar.Value = 0;
-            listview_scanner.Items.Clear();
-
+            this.Invoke(new Action(delegate
+            {
+                progress_bar.Maximum = EndPort - StartPort + 1;
+                progress_bar.Value = 0;
+                listview_scanner.Items.Clear();
+            }));
 
             IPAddress IpAddr = IPAddress.Parse(hostBox.Text);
             for (i = StartPort; i <= EndPort; i++)
             {
                 //Создаем сокет
                 IPEndPoint IpEndP = new IPEndPoint(IpAddr, i);
-                Socket MySoc = new Socket(AddressFamily.InterNetwork,
-                                         SocketType.Stream, ProtocolType.Tcp);
+                Socket MySoc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 //Пробуем подключится к указанному хосту
-                IAsyncResult asyncResult = MySoc.BeginConnect(IpEndP,
-                                 new AsyncCallback(ConnectCallback), MySoc);
-
+                IAsyncResult asyncResult = MySoc.BeginConnect(IpEndP, new AsyncCallback(ConnectCallback), MySoc);
 
                 if (!asyncResult.AsyncWaitHandle.WaitOne(30, false))
                 {
                     MySoc.Close();
-                    listview_scanner.Items.Add("Порт " + i.ToString());
-                    listview_scanner.Items[i - StartPort].SubItems.Add("закрыт");
-                    listview_scanner.Items[i - StartPort].BackColor = Color.Bisque;
-                    progress_bar.Value += 1;
+                    if (isJustOpenPorts.Checked) continue;
+                    this.Invoke(new Action(delegate
+                    {
+                        progress_bar.Value += 1;
+                        listview_scanner.Items.Add("Порт " + i.ToString());
+                        listview_scanner.Items[j].SubItems.Add("закрыт");
+                        listview_scanner.Items[j].BackColor = Color.Bisque;
+                        j++;
+                    }));
                 }
                 else
                 {
                     MySoc.Close();
-                    listview_scanner.Items.Add("Порт " + i.ToString());
-                    listview_scanner.Items[i - StartPort].SubItems.Add("открыт");
-                    listview_scanner.Items[i - StartPort].BackColor = Color.LightGreen;
-                    progress_bar.Value += 1;
+                    this.Invoke(new Action(delegate
+                    {
+                        progress_bar.Value += 1;
+                        listview_scanner.Items.Add("Порт " + i.ToString());
+                        listview_scanner.Items[j].SubItems.Add("открыт");
+                        listview_scanner.Items[j].BackColor = Color.LightGreen;
+                        j++;
+                    }));
                 }
             }
 
-            progress_bar.Value = 0;
+            this.Invoke(new Action(delegate
+            {
+                progress_bar.Value = 0;
+                scan_button.Text = "Сканировать";
+            }));
+            Thread.CurrentThread.Abort();
         }
 
         private static void ConnectCallback(IAsyncResult ar)
@@ -88,13 +104,22 @@ namespace PortScanner
             }
             catch (Exception e)
             {
-
             }
         }
 
         private void scan_button_Click(object sender, EventArgs e)
         {
-            Scan();
+            isStart = !isStart;
+            if (!isStart)
+            {
+                scan_button.Text = "Сканировать";
+                scanThread.Abort();
+            }
+            else
+            {
+                scan_button.Text = "Остановить";
+                scanThread.Start();
+            }
         }
 
         private void isScanAllPorts_CheckedChanged(object sender, EventArgs e)
@@ -102,6 +127,5 @@ namespace PortScanner
             portsTo.Enabled = !portsTo.Enabled;
             portsFrom.Enabled = !portsFrom.Enabled;
         }
-
     }
 }
